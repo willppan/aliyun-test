4a5de1fe9435af3c99109336c9a18f2ee82a9d30
panwei <1377853927@qq.com>
2021-02-07 18:21:32 +0800

    中邮

--- oly/js/common.js
+++
@@ -1,3 +0,0 @@
-﻿// JavaScript Document
-jQuery(".nav").slide({type:"menu",titCell:".nLi",targetCell:".sub",effect:"slideDown",delayTime:300,triggerTime:0,returnDefault:true});
-jQuery(".sider").slide({titCell:"h3",targetCell:"ul",effect:"slideDown",trigger:"click",delayTime:300,triggerTime:150,defaultPlay:true,returnDefault:true});
